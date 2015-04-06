ebeff58e3cf25caf10d041fb702a0acd5145bd0e
Jerson Carranza Lucas <jerson_carranza2@hotmail.com>
2015-04-05 20:01:27 -0500

    arreglos de iconos

--- public/js/filesFactory.js
+++ public/js/filesFactory.js
@@ -6,14 +6,14 @@ angular.module('filesFactory', [])
               method: 'GET',
               //url: 'http://127.0.0.1:5000/files/list/',
               url: 'https://snatlybackend.herokuapp.com/files/list',
-              cache: false
+              cache: true
             }).success(callback);
           },
           find: function(id, callback){
             $http({
               method: 'GET',
               url: 'https://snatlybackend.herokuapp.com/files/see/'+id,
-              cache: false
+              cache: true
             }).success(callback);
           }
         };
